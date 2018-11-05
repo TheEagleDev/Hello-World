5b60f24f5dd9e2dc71224d6f4e582f657c0ed7e3
TheEagleDev <44765451+TheEagleDev@users.noreply.github.com>
2018-11-06 01:15:37 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,1 @@
-from flask import  Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "hello"
-
-@app.route('/home')
-def home2():
-    return 'hello2'
-
-if __name__ == "__main__":
-        print("Hello world")
-        #app.run()
+print("Hello world")
